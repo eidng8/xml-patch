b5f9162aa47ca8545878b01018e1db7518dcc08a
eidng8 <cheung.jackey@gmail.com>
2020-03-25 12:25:27 +0800

    loop through all sample files for testing

--- tests/patcher.test.ts
+++ tests/patcher.test.ts
@@ -1,13 +1,43 @@
-import {readFileSync} from 'fs';
+import {existsSync, readdirSync, readFileSync} from 'fs';
+import {join} from 'path';
 import {Patcher} from '../src';
 import './helpers';
 
-test('it', () => {
-  const xml = readFileSync('tests/data/1A.xml', {encoding: 'utf-8'});
-  const diff = readFileSync('tests/data/1A.diff.xml', {encoding: 'utf-8'});
-  const patcher = new Patcher();
-  patcher.load(diff);
-  const patched = patcher.patch(xml);
-  expect(patched)
-    .toEqualXml(readFileSync('tests/data/1B.xml', {encoding: 'utf-8'}));
+describe('Sample XML', () => {
+  const [sources, fixtures, patches] = findSamples();
+  for (let idx = 0; idx < sources.length; idx++) {
+    const source = sources[idx];
+    const fixture = fixtures[idx];
+    const patch = patches[idx];
+
+    test(`it patches ${source}`, () => {
+      const xml = readFileSync(source, {encoding: 'utf-8'});
+      const diff = readFileSync(patch, {encoding: 'utf-8'});
+      expect(new Patcher().load(diff).patch(xml))
+        .toEqualXml(readFileSync(fixture, {encoding: 'utf-8'}));
+    });
+  }
 });
+
+function findSamples() {
+  const dir = 'tests/data';
+  const sources = [];
+  const fixtures = [];
+  const patches = [];
+  const regex = /^.+A.xml$/;
+  const files = readdirSync(dir, {withFileTypes: true});
+  for (const file of files) {
+    if (!file.isFile() || !regex.test(file.name)) {
+      continue;
+    }
+    const prefix = file.name.substr(0, file.name.length - 5);
+    const patch = join(dir, `${prefix}A.diff.xml`);
+    if (!existsSync(patch)) {
+      continue;
+    }
+    sources.push(join(dir, file.name));
+    fixtures.push(join(dir, `${prefix}B.xml`));
+    patches.push(patch);
+  }
+  return [sources, fixtures, patches];
+}
