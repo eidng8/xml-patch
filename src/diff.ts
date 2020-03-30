82fcd0a85f62e267f7bc625bfc3062aed939a9b4
eidng8 <cheung.jackey@gmail.com>
2020-03-30 18:03:06 +0800

    fooling around

--- src/diff.ts
+++ src/diff.ts
@@ -2,6 +2,10 @@ import {XMLFile} from './xml-file';
 import {ElementImpl} from 'xmldom-ts';
 
 export default class Diff {
+  static readonly NamespaceRegistry = 'urn:ietf:params:xml:ns:patch-ops-error';
+
+  static readonly SupportedActions = ['add', 'remove', 'replace'];
+
   protected xml!: XMLFile;
 
   protected _actions: ElementImpl[];
@@ -29,13 +33,14 @@ export default class Diff {
     if (!root) return this;
     for (const action of root.childNodes) {
       if (!XMLFile.isElement(action)) continue;
+      if (Diff.SupportedActions.indexOf(action.localName) < 0) continue;
       this._actions.push(action);
     }
     return this;
   }
 
   protected compileActions(): Diff {
-    for (const action of this._actions) {
+    for (const action of this.actions) {
       const exp = action.getAttribute('sel');
       if (!exp) continue;
       const cmp = this.compileAction(exp, action);
@@ -46,64 +51,137 @@ export default class Diff {
     return this;
   }
 
+  /**
+   * Handles namespace mangling of the given expression. It processes the given
+   * expression segment by segment, delimited by forward slash character `'/'`.
+   * @param exp the XPath expression
+   * @param action the action element
+   */
   protected compileAction(exp: string, action: ElementImpl): string {
     let idx = 0;
-    let depth = 0;
+    let isAttr = false;
+    let squareDepth = 0;
+    let bracketDepth = 0;
     let compiled = '';
     let part = '';
     let ch = '';
     let prefix = '';
 
-    const advance = () => {
-      compiled += this.compileNamespace(prefix, part, action) + ch;
+    const mangle = () => {
+      compiled += this.mangleElementNamespace(
+        prefix.trim(),
+        part.trim(),
+        isAttr,
+        action,
+      ) + ch;
       idx++;
       part = '';
       prefix = '';
+      isAttr = false;
+    };
+
+    const pack = () => {
+      compiled += prefix + part + ch;
+      prefix = '';
+      part = '';
+      ch = '';
+      isAttr = false;
     };
 
     while ((ch = exp[idx])) {
       switch (ch) {
+        // a segment is about to start, send everything before it to mangle.
         case '/':
-          advance();
+          mangle();
           continue;
 
+        // start of attribute expression
+        case '@':
+          isAttr = true;
+          compiled += '@';
+          ch = '';
+          break;
+
+        // if inside an attribute expression, mangle it
+        case '=':
+          if (isAttr) mangle();
+          break;
+
+        // stores current portion to prefix, if not in bracket or square bracket
         case ':':
-          if (depth) break;
+          if (squareDepth || bracketDepth) break;
           if (':' != exp[idx + 1]) {
             prefix = part;
             part = '';
+            idx++;
             continue;
           }
           break;
 
+        // send everything to mangle if this is the 1st opening square bracket
         case '[':
-          depth++;
-          advance();
+          if (isAttr || !squareDepth) mangle();
+          squareDepth++;
           continue;
 
+        // closes a square bracket pair, if there is no square bracket left open
+        // then pack everything to the compiled string, we don't mangle things
+        // inside square brackets
         case ']':
-          depth--;
+          squareDepth--;
+          if (!squareDepth) pack();
+          break;
+
+        // we don't do anything within brackets, just mark the its start
+        case '(':
+          bracketDepth++;
+          break;
+
+        // closes a bracket pair, if there is no bracket left open then pack
+        // everything to the compiled string, we don't mangle things inside
+        // brackets
+        case ')':
+          bracketDepth--;
+          if (!bracketDepth) pack();
           break;
       }
       part += ch;
       idx++;
     }
-    if (part) {
-      compiled += this.compileNamespace(prefix, part, action);
-    }
+    compiled += this.mangleElementNamespace(
+      prefix.trim(),
+      part.trim(),
+      isAttr,
+      action,
+    );
     return compiled;
   }
 
-  protected compileNamespace(
+  protected mangleElementNamespace(
     prefix: string,
     name: string,
+    isAttr: boolean,
     action: ElementImpl,
   ): string {
+    if (!name) return '';
+
+    // nothing needs done if the diff document is not namespaced
     if (!prefix && !action.namespaceURI) return name;
-    const ns = prefix ? action.lookupNamespaceURI(prefix) : action.namespaceURI;
-    if (!ns) {
-      return name;
+
+    // if the expression is a single wildcard, we don't need to do anything
+    if ('*' == name) return '*';
+
+    // find out the namespace URI for the diff document, so we can ignore it
+    // later on encounter
+    const ns = isAttr && !prefix ? '' : action.lookupNamespaceURI(prefix || '');
+
+    // expand the expression
+    let exp = '*';
+    if (ns) {
+      exp += `[namespace-uri()='${ns}']`;
     }
-    return `*[namespace-uri()='${ns}'][local-name()='${name}']`;
+    exp += `[local-name()='${name}']`;
+
+    return exp;
   }
 }
--- tests/diff.test.ts
+++ tests/diff.test.ts
@@ -1,11 +1,58 @@
 import {Diff, XMLFile} from '../src';
 
 describe('Diff', () => {
-  test('it will not change expression without namespace', () => {
+  test('it will not change expression without namespace', async () => {
     expect.assertions(1);
-    return new XMLFile().fromFile('tests/data/1A.diff.xml').then(xml => {
-      const diff = new Diff(xml);
-      expect(diff.actions[0].getAttribute('sel')).toBe('/a/b');
-    });
+    const xml = new XMLFile();
+    await xml.fromFile('tests/data/1A.diff.xml');
+    const diff = new Diff(xml);
+    expect(diff.actions[0].getAttribute('sel')).toBe('/a/b');
+  });
+
+  test('it mangles diff namespace', async () => {
+    expect.assertions(8);
+    const xml = new XMLFile();
+    await xml.fromFile('tests/data/rfc-a18.diff.xml');
+    const diff = new Diff(xml);
+    let action = diff.actions[0];
+    // * XPath query should be expanded using default namespace
+    // * attribute should be left untouched
+    expect(action.localName).toBe('add');
+    // @formatter:off
+    expect(action.getAttribute('sel')).toBe(
+      "*[namespace-uri()='urn:ietf:params:xml:ns:xxx'][local-name()='doc']/*[namespace-uri()='urn:ietf:params:xml:ns:xxx'][local-name()='elem'][@*[local-name()='a']='foo']",
+    );
+    // @formatter:on
+    action = diff.actions[1];
+    // * XPath query should be expanded using default namespace
+    // * function should be left untouched
+    expect(action.localName).toBe('replace');
+    // @formatter:off
+    expect(action.getAttribute('sel')).toBe(
+      "*[namespace-uri()='urn:ietf:params:xml:ns:xxx'][local-name()='doc']/*[namespace-uri()='urn:ietf:params:xml:ns:xxx'][local-name()='note']/text()",
+    );
+    // @formatter:on
+    action = diff.actions[2];
+    // * XPath query should be expanded using default namespace
+    // * XPath query should be expanded using namespace specified by prefix
+    // * asterisks should be left untouched
+    // * attribute should be left untouched
+    // * function should be left untouched
+    expect(action.localName).toBe('remove');
+    // @formatter:off
+    expect(action.getAttribute('sel')).toBe(
+      "*/*[namespace-uri()='urn:ietf:params:xml:ns:xxx'][local-name()='elem'][@*[local-name()='a']='bar']/*[namespace-uri()='urn:ietf:params:xml:ns:yyy'][local-name()='child']",
+    );
+    // @formatter:on
+    action = diff.actions[3];
+    // * XPath query should be expanded using default namespace
+    // * asterisks should be left untouched
+    // * attribute should be left untouched
+    expect(action.localName).toBe('add');
+    // @formatter:off
+    expect(action.getAttribute('sel')).toBe(
+      "*/*[namespace-uri()='urn:ietf:params:xml:ns:xxx'][local-name()='elem'][@*[local-name()='a']='bar']",
+    );
+    // @formatter:on
   });
 });
